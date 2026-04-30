11bc1422b9b7d4cbff9e7e2179bfa9d3355494bb
Lucia Terres <luciaterresf@gmail.com>
2026-04-30 19:07:05 +0200

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.append(str(PROJECT_ROOT))
+
 from src.config import (
     RESTAURANT_RANKING_PATH,
     CLEAN_DATA_PATH,
